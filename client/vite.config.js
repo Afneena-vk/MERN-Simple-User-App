ecb4f2036de4c1047e32ec519e1f5b527b56e308
Afneena-vk <afneenarahman98@gmail.com>
2025-03-17 05:09:24 +0530

    install react js and tailwindcss and create the first template

---
+++ client/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss()],
+//     },
+//   },
+// })
+
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+
+
