59577b08d28c8e5608e9af7117cbc90bba1ff2bd
Afneena-vk <afneenarahman98@gmail.com>
2025-03-18 02:12:27 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,11 +15,12 @@ mongoose.connect(process.env.MONGO)
 });
 
 const app = express();
-//app.use(express.json());
+app.use(express.json());
 // app.use(cookieParser());
 
 app.listen(4000,() => {
     console.log('Server listening on port 4000');    
 });
 
-app.use('/api/user',userRoutes)
+app.use('/api/user',userRoutes);
+app.use("/api/auth", authRoutes);
