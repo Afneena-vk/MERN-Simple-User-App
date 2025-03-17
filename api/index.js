f2a506d39ef5bfdb2f058ef9b4c308c929707f11
Afneena-vk <afneenarahman98@gmail.com>
2025-03-17 20:47:19 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected to Mongodb")
+})
+.catch((err)=>{
+    console.log(err)
+});
 
 const app = express();
 // app.use(express.json());
 // app.use(cookieParser());
 
-app.listen(3000,() => {
+app.listen(4000,() => {
     console.log('Server listening on port 4000');    
 });
