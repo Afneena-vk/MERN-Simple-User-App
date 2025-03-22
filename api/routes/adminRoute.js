b11ce9fb71b63ac58a9cbd4608124cdea9030fdd
Afneena-vk <afneenarahman98@gmail.com>
2025-03-22 05:40:45 +0530

    Admin create user functionality

--- api/routes/adminRoute.js
+++ api/routes/adminRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import { verifyAdmin } from '../utils/verifyAdmin.js';
-import { getUsers, adminUpdateUser, adminDeleteUser } from '../controllers/adminController.js';
+import { getUsers, adminUpdateUser, adminDeleteUser, adminCreateUser } from '../controllers/adminController.js';
 
 const router = express.Router();
  
@@ -11,6 +11,7 @@ const router = express.Router();
  router.get('/users',getUsers)
  router.put('/user/update/:id',adminUpdateUser)
  router.delete('/user/delete/:id', adminDeleteUser)
+ router.post('/user/create', adminCreateUser)
 
  
  export default router;
